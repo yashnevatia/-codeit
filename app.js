1adcaf7d0a7dfa94309bca32bff39632c113e94e
tejasvi-mehra <tejasvi.mehra@gmail.com>
2017-09-22 20:09:05 +0800

    Added tejasvi route

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/aman', function(req, res){
   res.send("hello from aman")
 })
 
+app.get('/tejasvi', function(req, res)
+{
+  res.send("Hello new")
+})
+
 app.listen(3000, function(){
   console.log("server successfully listening on port 3000");
 })
