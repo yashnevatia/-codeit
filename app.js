9ab9f66ddc5f72c52b6588bbed5b111155681b7f
Yashvardhan Nevatia <yashvardhannevatia@Yashvardhans-MacBook-Pro.local>
2017-09-23 09:48:04 +0800

    here

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 var express = require('express');
 var app = express();
-
-
-var PythonShell = require('python-shell');
-
-var options = {
-  mode: 'text',
-  pythonOptions: ['-u'],
-  scriptPath: './',
-  args: ['value1', 'value2', 'value3']
-};
+var _ = require('underscore');
 
 app.post('/sort', function(req, res){
-  console.log(req.body);
-})
+res.send("hey");
 
-// PythonShell.run('hello.py', options, function (err, results) {
-//   if (err) throw err;
-//   // results is an array consisting of messages collected during execution
-//   console.log('results: %j', results);
-// });
+});
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("server successfully listening on port 3000");
+  console.log("server successfully listening");
 })
